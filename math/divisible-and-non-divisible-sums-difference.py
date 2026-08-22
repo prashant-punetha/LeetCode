0d04b578cc0a495ab778e7430fbf4c87f5d690a0
Prashant <prashantpunetha74@gmail.com>
2026-08-22 21:50:48 +0530

    LC-divisible-and-non-divisible-sums-difference [Runtime: N/A; Memory: N/A]

---
+++ math/divisible-and-non-divisible-sums-difference.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def differenceOfSums(self, n, m):
+        num1=0
+        num2=0
+        for i in range(1,n+1):
+                if i%m==0:
+                    num1+=i
+                else:
+                    num2+=i
+        return (num2-num1)
+        
