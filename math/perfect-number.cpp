0d75201eadcf694f42967c59179b13a8fac7a11d
Prashant <prashantpunetha74@gmail.com>
2026-08-24 20:38:24 +0530

    LC-perfect-number [Runtime: N/A; Memory: N/A]

---
+++ math/perfect-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<num;i++)
+        {   
+            if(num%i==0)
+            {
+                sum+=i;
+            }
+        
+        }
+        if(num==sum)
+        {
+            return true;
+        }
+        return false;
+    
+        
+        
+    }
+};
