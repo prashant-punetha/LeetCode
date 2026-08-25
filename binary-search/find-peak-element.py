fce3edb87fd3ac21fe79a055fb28f75b124f4115
Prashant <prashantpunetha74@gmail.com>
2026-08-25 20:05:42 +0530

    LC-find-peak-element [Runtime: N/A; Memory: N/A]

--- binary-search/find-peak-element.py
+++ binary-search/find-peak-element.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def findPeakElement(self, nums):
         max=nums[0]
-        for i in nums:
-            if nums[i]>max:
+        for i in range(len(nums)):
+            if nums[i]>=max:
                 max=nums[i]
                 index=i
         return index
