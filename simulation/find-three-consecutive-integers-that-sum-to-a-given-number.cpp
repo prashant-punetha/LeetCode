730c996253247864dca4b742708062278fbbae76
Prashant <prashantpunetha74@gmail.com>
2026-08-27 20:59:54 +0530

    LC-find-three-consecutive-integers-that-sum-to-a-given-number [Runtime: N/A; Memory: N/A]

--- simulation/find-three-consecutive-integers-that-sum-to-a-given-number.cpp
+++ simulation/find-three-consecutive-integers-that-sum-to-a-given-number.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     vector<long long> sumOfThree(long long n) {
+
        
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    for (int k = 0; k < n; k++) {
-                        if (i + j + k == n &&(j==i+1&&k==j+1)) {
-                            return {i, j, k};
-                        }
-                    }
-                }
+            if (n % 3 != 0) {
+                return {};
             }
+            long long mid=n/3;
         
-        return {};
+        return {mid-1,mid,mid+1};
     }
 };
