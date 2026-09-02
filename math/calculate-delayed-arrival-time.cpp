429ce210799f4a7db59e8cf34f87359e09a0fd8a
Prashant <prashantpunetha74@gmail.com>
2026-09-02 10:43:15 +0530

    LC-calculate-delayed-arrival-time [Runtime: 0 ms; Memory: 8.3 MB]

--- math/calculate-delayed-arrival-time.cpp
+++ math/calculate-delayed-arrival-time.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int findDelayedArrivalTime(int aTime, int dTime) {
-        if(aTime+dTime>=24)
-        {
-            return 0;
-
-        }
-        else
-        {
-            return aTime+dTime;
-        }
+       
+      
+                return (aTime+dTime)%24;
+       
         
     }
 };
