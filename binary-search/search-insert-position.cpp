2fff397e188185a61293899e3add1fef7d7846c0
Prashant <prashantpunetha74@gmail.com>
2026-09-06 21:06:16 +0530

    LC-search-insert-position [Runtime: N/A; Memory: N/A]

---
+++ binary-search/search-insert-position.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int index=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<target){
+                index=i+1;
+            }
+            if(nums[i]==target)
+            {
+                index=i;
+            }
+        }
+        return index;
+        
+    }
+};
