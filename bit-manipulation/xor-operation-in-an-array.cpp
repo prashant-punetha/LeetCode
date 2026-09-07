05011563c5e41c65760e8539ae2e953ed11865d2
Prashant <prashantpunetha74@gmail.com>
2026-09-07 21:47:31 +0530

    LC-xor-operation-in-an-array [Runtime: N/A; Memory: N/A]

---
+++ bit-manipulation/xor-operation-in-an-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int nums[n];
+        int x=0;
+        for(int i=0;i<n;i++)
+        {
+            
+            x^=start+(2*i);
+        }
+        return x;
+        
+    }
+};
