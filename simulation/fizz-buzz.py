a908492c2d2710e0238ba45767cb0849ef1670c2
Prashant <prashantpunetha74@gmail.com>
2026-09-12 21:44:34 +0530

    LC-fizz-buzz [Runtime: N/A; Memory: N/A]

--- simulation/fizz-buzz.py
+++ simulation/fizz-buzz.py
@@ -2,14 +2,12 @@ class Solution(object):
     def fizzBuzz(self, n):
         ans=[]
         for i in range(1,n+1):
-            if(i%3==0 and i%5==0):
-                ans.append("FizzBuzz")
-            elif(i%3==0):
-                ans.append("Fizz")
-            elif(i%5==0):
-                ans.append("Buzz")
-            else:
-                ans.append(str(i))
+           ans.append(
+            "FizzBuzz" if i%15==0 else
+            "Buzz" if i%5==0 else
+            "Fizz" if i%3==0 else
+            str(i)
+           )
         return ans
 
 
