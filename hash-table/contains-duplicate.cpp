151269ad30842cc170a5415fd8d346e4f8fea6c8
Prashant <prashantpunetha74@gmail.com>
2026-09-13 19:45:12 +0530

    LC-contains-duplicate [Runtime: N/A; Memory: N/A]

--- hash-table/contains-duplicate.cpp
+++ hash-table/contains-duplicate.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        vector<int> dub(nums.size());
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i+1; j < nums.size(); j++) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
+
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
             }
         }
         return false;
