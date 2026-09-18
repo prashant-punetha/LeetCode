f993015f75ef891c48b5a8906303f49cb0ee04ce
Prashant <prashantpunetha74@gmail.com>
2026-09-18 21:15:49 +0530

    LC-find-all-numbers-disappeared-in-an-array [Runtime: N/A; Memory: N/A]

--- hash-table/find-all-numbers-disappeared-in-an-array.py
+++ hash-table/find-all-numbers-disappeared-in-an-array.py
@@ -1,7 +1,4 @@
 class Solution(object):
     def findDisappearedNumbers(self, nums):
-        ans=[]
-        for i in range(1,len(nums)+1):
-            if not i in nums:
-                ans.append(i)
-        return ans
+        s = set(nums)  
+        return [i for i in range(1, len(nums) + 1) if i not in s]
